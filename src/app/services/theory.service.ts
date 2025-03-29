38f11140ba9bef6fcd729299ec1548eb9a29330b
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2025-03-29 10:12:33 -0500

    database included

--- src/app/services/theory.service.ts
+++ src/app/services/theory.service.ts
@@ -9,49 +9,28 @@ export interface TopicCardData {
     bodyStyle: { [key: string]: string };
 }
 
+export interface ChoiceCardData {
+    txt: string;
+    correctness: number;
+}
+
+export interface QuestionCardData {
+    txt: string;
+    choices: ChoiceCardData[];
+}
+
 @Injectable({
     providedIn: 'root'
 })
 export class TheoryService {
-    public topics: TopicCardData[] = [
-        {
-            id: "1",
-            title: "Comunicaciones Aeronáuticas",
-            description: "Comunicaciones Aeronauticas",
-            backgroundStyle: {
-                "background-color": "#aee8ff",
-            },
-            bodyStyle: {
-                "background-image": 'url("https://storage.googleapis.com/labs-pro-public/svg/car.svg")',
-            }
-        },
-        {
-            id: "2",
-            title: "Comunicaciones Aeronáuticas",
-            description: "Comunicaciones Aeronauticas",
-            backgroundStyle: {
-                "background-color": "rgb(212 255 193)",
-            },
-            bodyStyle: {
-                "background-image": 'url("https://storage.googleapis.com/labs-pro-public/svg/mountain.svg")',
-            }
-        },
-        {
-            id: "3",
-            title: "Comunicaciones Aeronáuticas",
-            description: "Comunicaciones Aeronauticas",
-            backgroundStyle: {
-                "background-color": "#ffe3c7",
-            },
-            bodyStyle: {
-                "background-image": 'url("https://storage.googleapis.com/labs-pro-public/svg/volcano.svg")',
-            }
-        }
-    ];
 
     constructor(private httpSrv: HttpService) { }
 
-    async getDatabase() {
-        return this.topics;
+    async getDatabase(): Promise<any> {
+        return this.httpSrv.get("assets/db/topics.json");
+    }
+
+    async getQuestions(fileName: string): Promise<any> {
+        return this.httpSrv.get(`assets/db/${fileName}`);
     }
 }
---
+++ src/assets/db/process.mjs
@@ -0,0 +1,40 @@
+import fs from "fs";
+
+let content = fs.readFileSync("./rawComunicacionesAeronauticas.txt", { encoding: "utf8" });
+
+let state = "question";
+const lines = content.split(/[\n\r]/g);
+
+const questions = [];
+let currentQuestion = null;
+for (let i = 0; i < lines.length; i++) {
+    const line = lines[i].trim();
+    if (line.length > 0) {
+        if (state == "question") {
+            currentQuestion = {
+                txt: line,
+                choices: [],
+            };
+            questions.push(currentQuestion);
+            state = "choices";
+        } else if (state == "choices") {
+            if (line.includes("Respuesta opción")) {
+                state = "answer";
+            } else {
+                currentQuestion.choices.push({
+                    txt: line,
+                    correctness: 0,
+                });
+            }
+        } else if (state == "answer") {
+            const answer = parseInt(line);
+            if (isNaN(answer)) {
+                console.log(JSON.stringify(questions, null, 4));
+                throw new Error(`At line ${i} a number was expected but found ${line}`);
+            }
+            state = "question";
+        }
+    }
+}
+
+fs.writeFileSync("topic_1.json", JSON.stringify(questions, null, 4), { encoding: "utf8" });
