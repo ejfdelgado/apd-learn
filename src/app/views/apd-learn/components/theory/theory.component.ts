ff84273c55aa030151bd93fd43f886a5af623f69
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2025-03-29 09:39:36 -0500

    layout1 ok

--- src/app/views/apd-learn/components/theory/theory.component.ts
+++ src/app/views/apd-learn/components/theory/theory.component.ts
@@ -4,7 +4,7 @@ export interface TopicCardData {
   title: string;
   description: string;
   backgroundStyle: { [key: string]: string };
-  centerImageURL: string;
+  bodyStyle: { [key: string]: string };
 }
 
 @Component({
@@ -24,20 +24,32 @@ export class TheoryComponent {
     {
       title: "Comunicaciones Aeronáuticas",
       description: "Comunicaciones Aeronauticas",
-      backgroundStyle: {},
-      centerImageURL: ""
+      backgroundStyle: {
+        "background-color": "#aee8ff",
+      },
+      bodyStyle: {
+        "background-image": 'url("https://storage.googleapis.com/labs-pro-public/svg/car.svg")',
+      }
     },
     {
       title: "Comunicaciones Aeronáuticas",
       description: "Comunicaciones Aeronauticas",
-      backgroundStyle: {},
-      centerImageURL: ""
+      backgroundStyle: {
+        "background-color": "rgb(212 255 193)",
+      },
+      bodyStyle: {
+        "background-image": 'url("https://storage.googleapis.com/labs-pro-public/svg/mountain.svg")',
+      }
     },
     {
       title: "Comunicaciones Aeronáuticas",
       description: "Comunicaciones Aeronauticas",
-      backgroundStyle: {},
-      centerImageURL: ""
+      backgroundStyle: {
+        "background-color": "#ffe3c7",
+      },
+      bodyStyle: {
+        "background-image": 'url("https://storage.googleapis.com/labs-pro-public/svg/volcano.svg")',
+      }
     }
   ];
 
